e213c4ef024b967a637ecd621b408575ba16dcae
nicolacalisedevelhope <nicolacalise3355@gmail.com>
2024-06-29 10:10:25 +0200

    update

---
+++ backend/app/src/main/java/com/nicolacalise/ApplicationBackend/models/auth/AuthRequest.java
@@ -0,0 +1,30 @@
+package com.nicolacalise.ApplicationBackend.models.auth;
+
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+    public AuthRequest() {}
+
+    public AuthRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
